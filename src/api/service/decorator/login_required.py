d1b89840602c8b424df139ec578bb36c72ffe145
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2020-09-17 16:02:22 +0300

    login_required refactoring

--- src/api/service/decorator/login_required.py
+++ src/api/service/decorator/login_required.py
@@ -1,8 +1,12 @@
-from aiohttp import web
-import jwt, json, config
+import jwt
+import json
+import config
 from bson import ObjectId
-
-from cores.rest_core import codes, APIException
+from aiohttp import web
+from cores.rest_core import (
+    codes,
+    APIException
+)
 
 
 class ApiKeyException(APIException):
@@ -10,6 +14,24 @@ class ApiKeyException(APIException):
     @property
     def message(self):
         return 'API key expired. Please renew the API key.'
+
+    code = codes.BAD_REQUEST
+
+
+class ProfileInformationException(APIException):
+
+    @property
+    def message(self):
+        return 'Your name or surname is not filled. Please fill the information before.'
+
+    code = codes.BAD_REQUEST
+
+
+class BlockingException(APIException):
+
+    @property
+    def message(self):
+        return 'Your account is blocked!'
 
     code = codes.BAD_REQUEST
 
@@ -26,20 +48,23 @@ def login_required(func):
         except (jwt.DecodeError, jwt.ExpiredSignatureError):
             raise ApiKeyException()
 
-        session_id = payload['id']
-        user_id = payload['user_id']
+        data = json.loads(await request.app.session.get(payload['id']))
 
-        data = await request.app.session.get(session_id)
-        data = json.loads(data)
-
-        if data is None or user_id != data['user_id']:
+        if data is None:
+            raise ApiKeyException()
+        elif payload['user_id'] != data['user_id']:
             raise ApiKeyException()
 
         user = await request.app.db.users.find_one({'_id': ObjectId(data['user_id'])})
         if user is None:
             raise ApiKeyException()
+        elif 'name' not in user or 'surname' not in user or 'blocked' not in user:
+            raise ProfileInformationException()
+        elif user['blocked'] is True:
+            raise BlockingException()
+        elif user['name'] is None or user['surname'] is None:
+            raise ProfileInformationException()
 
         request.user = user
         return await func(request)
-
     return wrapped
