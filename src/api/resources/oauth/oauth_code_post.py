39384d745b9091e2731edb2be228adc5f1c90ee2
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2020-11-19 01:21:31 +0100

    oauth_code_post.py refactoring

--- src/api/resources/oauth/oauth_code_post.py
+++ src/api/resources/oauth/oauth_code_post.py
@@ -1,88 +1,87 @@
 import phonenumbers
 from phonenumbers import region_code_for_country_code
-from api.resources.oauth.schemas import (
-    SerializationNumberCompleteSchema,
-    DeserializationCodeSchema
-)
 from api.service import OAuthSession
 from api.service.session import (
-    UserSession,
-    JWTToken
+    create_tokens
+)
+from cores.marshmallow_core import (
+    ApiSchema,
+    fields
 )
 from cores.rest_core import (
     codes,
     APIException
 )
-from models import Users
-from models.users import LastCoordinates
+from models import (
+    Users,
+    LastCoordinates
+)
 from utils import (
     coordinates_from_region_code
 )
 
 
-class ExpiredException(APIException):
+class TimeExpiredException(APIException):
 
     @property
     def message(self):
-        return 'Authentication time has expired. Please start again from the launcher'
+        return 'Authentication time has expired.'
 
     code = codes.AUTHORIZATION_EXPIRED
 
 
-class SMSCodeException(APIException):
+class CodeException(APIException):
 
     @property
     def message(self):
-        return 'SMS Verification Code is Invalid'
+        return 'Invalid verification code.'
 
     code = codes.BAD_REQUEST
 
 
-"""
-    Error exceptions:
-        - 419: Authentication time has expired. Please start again from the launcher
-        - 400: DataError exception
-"""
+class DeserializationSchema(ApiSchema):
+    number = fields.PhoneNumber(required=True)
+    verify_key = fields.Str(required=True)
+    code = fields.Str(required=True)
+
+
+class SerializationSchema(ApiSchema):
+    access_token = fields.Str(default=None)
+    refresh_token = fields.Str(default=None)
+    expires_in = fields.Timestamp(default=None)
 
 
 async def OAuthCodePost(request):
-    data = DeserializationCodeSchema().deserialize(await request.json())
-    session = OAuthSession(data['number'], app=request.app)
+    app = request.app
+    data = DeserializationSchema().deserialize(await request.json())
+    session = OAuthSession(key=data['number'],
+                           app=app)
 
     if not await session.is_exists():
-        raise ExpiredException()
+        raise TimeExpiredException()
 
     result, expires_in = await session.get_data()
     if result['verify_key'] != data['verify_key'] or result['code'] != data['code']:
-        raise SMSCodeException()
-
+        raise CodeException()
     await session.destroy()
-    phone_number = phonenumbers.parse(data['number'], None, _check_region=True)
-    region_code = region_code_for_country_code(phone_number.country_code)
+
+    phone_number = phonenumbers.parse(number=data['number'],
+                                      _check_region=True)
+    region_code = region_code_for_country_code(country_code=phone_number.country_code)
 
     user = await Users.find_one({'phone': data['number']})
     if user is None:
-        lat, long = coordinates_from_region_code(region_code)
-
         user = Users()
         user.phone = data['number']
         user.region_code = region_code
         user.blocked = False
         user.last_coord = LastCoordinates()
+
+        lat, long = coordinates_from_region_code(region_code)
         user.last_coord.lat = lat
         user.last_coord.long = long
+        await user.commit()
 
-        try:
-            await user.commit()
-        except Exception as e:
-            raise ExpiredException()
-
-    session = await UserSession(request.app).create_session(user)
-    access_token, expires_in = JWTToken().generate_access(session)
-    refresh_token, refresh_expires_in = JWTToken().generate_refresh(session)
-    result = {
-        'access_token': access_token,
-        'refresh_token': refresh_token,
-        'expires_in': expires_in
-    }
-    return SerializationNumberCompleteSchema().serialize(result)
+    response = create_tokens(app=app,
+                             user=user)
+    return SerializationSchema().serialize(response)
